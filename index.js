dbfbd287f271ac81098dd83bdb4f44c084e9ee12
Ck-kosh <elias.cheruiyot@student.moringaschool.com>
2026-04-14 14:53:35 +0300

    arrow

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
-
+function createLoginTracker (userInfo, username, password){
+  let attemptCount = 0
+  return (passwordAttempt) =>(
+    attemptCount++;
+    if(passwordAttempt === userInfo.password){
+      if (attemptCount <= 3){
+        console.log("Login successful")
+      }
+      else{
+        console.log("Account locked due to too many failed login attempts")
+      }
+    }
+    else{
+      if(attemptCount <= 3){
+        console.log(`Attempt ${attemptCount}: Login failed`)
+      }
+      else{
+        console.log("Account locked due to too many failed login attempts")
+      }
+    }
+  )
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
